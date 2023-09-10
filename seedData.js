60ee1af8db487f1b537462e779dad3f767939eb5
Dima Rosmait <dima.rosmait@holi.team>
2023-09-10 12:19:17 +0200

    add seed data

---
+++ seedData.js
@@ -0,0 +1,42 @@
+export const DATA = [
+  {
+    id: 1,
+    text: "Card #1",
+    uri: "https://source.unsplash.com/random?1",
+  },
+  {
+    id: 2,
+    text: "Card #2",
+    uri: "https://source.unsplash.com/random?2",
+  },
+  {
+    id: 3,
+    text: "Card #3",
+    uri: "https://source.unsplash.com/random?3",
+  },
+  {
+    id: 4,
+    text: "Card #4",
+    uri: "https://source.unsplash.com/random?4",
+  },
+  {
+    id: 5,
+    text: "Card #5",
+    uri: "https://source.unsplash.com/random?5",
+  },
+  {
+    id: 6,
+    text: "Card #6",
+    uri: "https://source.unsplash.com/random?6",
+  },
+  {
+    id: 7,
+    text: "Card #7",
+    uri: "https://source.unsplash.com/random?7",
+  },
+  {
+    id: 8,
+    text: "Card #8",
+    uri: "https://source.unsplash.com/random?1",
+  },
+];
